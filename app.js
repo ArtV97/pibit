2b4e3e3c6f846bc1d689674f85243295b0444929
ArtV97 <arthurvianna@id.uff.br>
2020-09-17 19:32:35 -0300

    consign fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require("./config/server");
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-	port = 8000;
+	port = 3000;
 }
 
 app.listen(port, function(){
--- config/server.js
+++ config/server.js
@@ -11,13 +11,12 @@ app.set("views", "./app/views");
 app.use(express.static("./app/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-
 //auto-load
 consign()
-	.include("app/routes")
-	.then("app/models")
-	.then("app/controllers")
-	.then("config/googleSpreadsheetConnection.js")
+	.include(process.cwd() + "/app/routes")
+	.then(process.cwd() + "/app/models")
+	.then(process.cwd() + "/app/controllers")
+	.then(process.cwd() + "/config/googleSpreadsheetConnection.js")
 	.into(app);
 
 module.exports = app;
