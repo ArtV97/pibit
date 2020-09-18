26e15169b30b4e2dd0c60983bf2300cec98fc447
ArtV97 <arthurvianna@id.uff.br>
2020-09-17 22:18:22 -0300

    cosign fix

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,6 +1,7 @@
 module.exports.index = function(application, req, res){
 	var doc = application.config.googleSpreadsheetConnection;
-	var spreadSheetModels = new application.app.models.SpreadSheetModels(doc);
+	//var spreadSheetModels = new application.app.models.SpreadSheetModels(doc);
+	var spreadSheetModels = new application.models.SpreadSheetModels(doc);
 	spreadSheetModels.getRows(function(error, result){
 		res.render("index", {title: doc.title, rows: result});
 	});
