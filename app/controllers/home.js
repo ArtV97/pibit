4510faff6114ad8530e0a8a3345d598cc708f8b8
ArtV97 <arthurvianna@id.uff.br>
2021-09-14 10:54:48 -0300

    insert sensor_id

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,7 +1,7 @@
 module.exports.index = function(application, req, res){
 	var doc = application.config.googleSpreadsheetConnection;
-	//var spreadSheetModels = new application.app.models.SpreadSheetModels(doc);
-	var spreadSheetModels = new application.models.SpreadSheetModels(doc);
+	var spreadSheetModels = new application.app.models.SpreadSheetModels(doc);
+	//var spreadSheetModels = new application.models.SpreadSheetModels(doc);
 	spreadSheetModels.getRows(function(error, result){
 		res.render("index", {title: doc.title, rows: result});
 	});
@@ -9,10 +9,11 @@ module.exports.index = function(application, req, res){
 
 module.exports.addRow = function(application, req, res){
 	var doc = application.config.googleSpreadsheetConnection;
-	//var spreadSheetModels = new application.app.models.SpreadSheetModels(doc);
-	var spreadSheetModels = new application.models.SpreadSheetModels(doc);
+	var spreadSheetModels = new application.app.models.SpreadSheetModels(doc);
+	//var spreadSheetModels = new application.models.SpreadSheetModels(doc);
 	
 	spreadSheetModels.addRow(req.query, function(error){
+		console.log("Insert Data:", req.query)
 		res.redirect("/");
 	});
 };
